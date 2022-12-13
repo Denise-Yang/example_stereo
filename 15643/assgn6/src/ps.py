b2e7ecc0bc01a104a451688c1df376277dbc2437
Denise-Yang <47067329+Denise-Yang@users.noreply.github.com>
2022-12-12 23:01:20 -0500

    updated code

--- 15643/assgn6/src/ps.py
+++ 15643/assgn6/src/ps.py
@@ -12,14 +12,15 @@ resDir = 'submit'
 useLowRes = False #enable lowres for debugging
 dX = 558.8 #calibration plane length in x direction
 dY = 303.2125 #calibration plane length in y direction
+
 frogStart = [311,324]
 frogEnd = [809,630]
 vOffset = 250 
 hOffset = 190
 yOffset = 650
 
-upBound = 4000.
-loBound = -2000.
+upBound = 100.
+loBound = -2500.
 
 #For extracting images from video 
 def extract_images(pathIn, pathOut):
@@ -39,11 +40,7 @@ def get_shadow_im(ims):
     min = np.min(ims, axis=0)
     return (max + min)/2
 
-"""
-x1 y1 1     a/c     0
-x2 y2 1     b/c     0
-x3 y3 1     1       0
-"""
+
 def get_edge(im,h,w, vOffset, hOffset):
     #get both positive and negative edge and make sure accurate for all frames
     im_shifted = np.roll(im,-1,axis=1)
@@ -64,17 +61,6 @@ def get_edge(im,h,w, vOffset, hOffset):
     
     m = -s[0]/s[1]
     c = -1/s[1]
-    # if (h > 95):
-    #     x1 = points[0,1]
-    #     print(x1)
-    #     x2 =  points[len(points)-1,1]
-
-    #     xs,ys = [x1, x2], [m*x1+c,m*x2+c]
-    #     # x1= [points[0,1], points[len(points)-1,1]]
-    #     # y1= [points[0,0], points[len(points)-1,0]]
-    #     # plt.imshow(edge)
-    #     plt.plot(xs,ys, marker="o")
-    #     plt.show()
     return (m,c)
 
 
@@ -110,15 +96,6 @@ def getShadowPlane(v_line, h_line, mtx, dist, t_h, m_h, t_v, m_v,idx, delta_im,h
     p4 = [ vm*yv1+vc, yv1]
     pts = np.array([p1,p2,p3,p4])
 
-    # if (idx > 70):
-
-    #     xs,ys = [vm*yv1+vc,vm*yv0+vc],  [yv1, yv0]
-    #     xh,yh = [hm*(yh1-yOffset)+hc,hm*(yh0-yOffset)+hc],  [yh1, yh0]
-    #     plt.imshow(delta_im)
-    #     plt.plot(xs,ys, c='b', marker="o")
-    #     plt.plot(xh,yh, c='r', marker="o")
-    #     plt.show()
-
 
     #Use pixel2Ray to convert to cam world aka the dir
     cam_rays = pixel2ray(pts, mtx, dist)
@@ -133,41 +110,12 @@ def getShadowPlane(v_line, h_line, mtx, dist, t_h, m_h, t_v, m_v,idx, delta_im,h
 
             r = np.matmul(m_h.T, r0[0]) #cam2Plane(r0, t_h, m_h)
             t = -oh[2]/r[2]
-            Ps.append(plane2Cam(oh.T - r*t, t_h, m_h))
+            Ps.append(plane2Cam(oh.T + r*t, t_h, m_h))
         else:
             r = np.matmul(m_v.T, r0[0])
             t = -ov[2]/r[2] 
-            Ps.append(plane2Cam(ov.T - r*t, t_v, m_v))
-    # if(idx > 40):
-    #     y0 = 0
-    #     y1 = 100
-    #     y2 = 650
-    #     y3 = 750
-    #     xs,ys = [vm*y1+vc,vm*y0+vc],  [y1, y0]
-    #     xh,yh = [hm*(y2)+hc,hm*(y3)+hc],  [y2, y3],
-    #     z = [0,0]
-    #     plt.imshow(delta_im)
-    #     print(i)
-    #     plt.plot(xs,ys, c='b', marker="o")
-    #     plt.plot(xh,yh, c='r', marker="o")
-    #     plt.show()
-    #     # drawPlane(Ps[0].T[0], Ps[1].T[0],Ps[2].T[0],Ps[3].T[0])
-    #     p1 = Ps[0].T[0]
-    #     p2 = Ps[1].T[0]
-    #     p3 = Ps[2].T[0]
-    #     p4 = Ps[3].T[0]
-    #     fig = plt.figure()
-    #     ax = fig.add_subplot(projection='3d')
-    #     ax.plot([p1[0], p2[0]], [p1[1], p2[1]], zs=[p1[2], p2[2]], c='b') 
-    #     ax.plot([p2[0],p3[0]], [p2[1], p3[1]], zs=[p2[2], p3[2]], c='r') 
-    #     ax.plot([p3[0], p4[0]], [p3[1], p4[1]], zs=[p3[2], p4[2]], c='m') 
-    #     ax.plot([p4[0], p1[0]], [p4[1], p1[1]], zs=[p4[2], p1[2]], c='y') 
-    #     ax.plot(xs,ys,zs=z, c='g')
-    #     ax.plot(xh,yh,zs=z, c='c')
-    #     ax.set_xlabel('X')
-    #     ax.set_ylabel('Y')
-    #     ax.set_zlabel('Z')
-    #     plt.show()
+            Ps.append(plane2Cam(ov.T + r*t, t_v, m_v))
+
     return Ps
 
 def show3d(oneDArray,im):
@@ -180,7 +128,7 @@ def show3d(oneDArray,im):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     im = im[(mask[:,2]<upBound) * (mask[:,2]>loBound)]
-    ax.scatter3D(oneDArray[:,0], oneDArray[:,1], oneDArray[:,2],c=oneDArray[:,2],cmap='gray')
+    ax.scatter3D(oneDArray[:,0], oneDArray[:,1], oneDArray[:,2],c=im,cmap='gray')
     plt.title("Reconstruction")
     plt.show()
     
@@ -224,8 +172,8 @@ def getShadowPlaneIntersection(rays, planes, shadow_time, indices):
     rs = rays[Y,X] #np.array(np.take(rays, indices))
     
 
-    P1 = Ps[:,:,0]
-    P2 = Ps[:,:,1]
+    P1 = Ps[:,:,1]
+    P2 = Ps[:,:,0]
     P3 = Ps[:,:,2]
     P4 = Ps[:,:,3]
     
@@ -239,18 +187,13 @@ def getShadowPlaneIntersection(rays, planes, shadow_time, indices):
 
     times = np.sum(P1*n_norm, axis=2)/np.sum(rs*n_norm, axis=2)
     times = np.repeat(times, 3).reshape(h,w,3)
-    # for i in range(100,200):
-    #     for j in range(200,300):
-    #         drawPlane(P1[i][j],P2[i][j],P3[i][j],P4[i][j])
-    return rs*times
+    return -rs*times
     
 
 
 def createThresholdMask(ims):
-    diff = np.max(ims, axis=0) - np.min(ims, axis=0)
-    
+    diff = np.max(ims, axis=0) - np.min(ims, axis=0) 
     return (diff > .200) * (diff < .6)
-    # return np.where((ims > 50 ) * (ims < 140), 1, 0)
 
 def main():
     ims = []
@@ -292,11 +235,6 @@ def main():
         
 
     
-    shadow_planes_flat= np.array(shadow_planes).reshape((166*4,3))
-  
-    # show3d(shadow_planes_flat, None)
-    # np.savez(os.path.join(resDir, "shadowPlanePoints.npz"), planes=shadow_planes)
-
 
     #get shadow time
     shadow_vals = np.zeros((h,w))
@@ -336,37 +274,15 @@ def main():
     dims = threshold_mask.shape
     #use cropped indices, get shadow time valye then use that valuye to get the plane, then proceed as normal
     intersections = getShadowPlaneIntersection(rays, shadow_planes, shadow_time, cropped_idx)
-    writePlanesAndNorms(shadow_planes)
+    # writePlanesAndNorms(shadow_planes)
     intersections_flat = intersections.flatten().reshape((dims[0],3))
     validims = cropped_ims[0].flatten()
     valid_intersections = intersections_flat[threshold_mask]
     valid_colors = validims[threshold_mask]
     show3d(valid_intersections, valid_colors)
   
-
-
-
-
-    # plt.imshow(shadow_time, cmap= "jet", interpolation=None)
-    # plt.show()
-
-
-  
 if __name__ == "__main__":
     main()
 
 
 
-
-    ##code graveyard
-        #stuff for drawing shaodw edges on image
-        # xs,ys = [vm*x1+vc,vm*x2+vc],  [x1, x2]
-        # xh,yh = [hm*(x1-yOffset)+hc,hm*(x2-yOffset)+hc],  [x1, x2]
-
-        # # x1= [points[0,1], points[len(points)-1,1]]
-        # # y1= [points[0,0], points[len(points)-1,0]]
-        # plt.imshow(delta_im)
-        # print(i)
-        # plt.plot(xs,ys, c='b', marker="o")
-        # plt.plot(xh,yh, c='r', marker="o")
-        # plt.show()
